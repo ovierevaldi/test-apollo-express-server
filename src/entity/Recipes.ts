020cd074e1550dc4ab31db81529ab985d698ff87
Ovie Revaldi <ovierevaldi@gmail.com>
2024-12-31 14:03:57 +0700

    Add property for typeorm in Receipt Table

--- src/entity/Recipes.ts
+++ src/entity/Recipes.ts
@@ -1,19 +1,26 @@
 import { Field, GraphQLISODateTime, ID, InputType, ObjectType } from "type-graphql";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
+@Entity()
 @ObjectType()
 export default class Recipe {
+    @PrimaryGeneratedColumn()
     @Field(type => ID)
     id: string;
     
+    @Column({type: 'varchar', length: '50'})
     @Field(type => String)
     title: string;
 
+    @Column({type: 'text'})
     @Field(type => String, {nullable: true})
     description?: string;
     
+    @Column({type: 'date'})
     @Field(type => GraphQLISODateTime)
     creationDate: Date;
     
+    @Column("simple-array")
     @Field(type => [String])
     ingredients: string[];
 }
