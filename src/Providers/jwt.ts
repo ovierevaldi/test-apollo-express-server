7f44fb6767b895ae27e52c5ceb6a08406ae4d8b0
Ovie Revaldi <ovierevaldi@gmail.com>
2025-01-03 14:18:56 +0700

    Change JWT Provider Method
    Create function specific example: user, instead of just recreating generic rules

--- src/Providers/jwt.ts
+++ src/Providers/jwt.ts
@@ -1,17 +1,42 @@
 import jwt, { JwtPayload } from 'jsonwebtoken'
 
+type userPayload = {
+  id: string
+  username: string
+  roles: string[]
+}
+
 const jwtProvider = () => {
     const sign = (payload: object) => {
-      return jwt.sign(payload, process.env.JWT_SECRET, {expiresIn: "1d"});
+      if(process.env.JWT_SECRET)
+        return jwt.sign(payload, process.env.JWT_SECRET, {expiresIn: "1d"});
     };
 
     const verify = (token:string, callback: (err: jwt.VerifyErrors | null, decoded: string | JwtPayload | undefined) => void) => {
-        return jwt.verify(token, process.env.JWT_SECRET, function(err, decoded){
-           callback(err, decoded);
+        if(process.env.JWT_SECRET){
+          return jwt.verify(token, process.env.JWT_SECRET, function(err, decoded){
+            callback(err, decoded);
+         })
+        }
+    };
+
+    const signUser = (payload: userPayload) => {
+      return sign(payload);
+    };
+
+    const verifyUser = (token: string): Promise<userPayload | null> => {
+      return new Promise((resolve) => {
+        verify(token, (err, decoded) => {
+          if(decoded){
+            resolve(decoded as userPayload);
+          }
+          else
+            resolve(null);
         })
+      })
     }
 
-    return{ sign, verify }
+    return{ signUser, verifyUser }
 };
 
 export default jwtProvider;
