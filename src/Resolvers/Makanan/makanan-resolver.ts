006154513edf74e3f655052e24ea73935135fb5e
Ovie Revaldi <ovierevaldi@gmail.com>
2025-01-10 08:49:20 +0700

    add skip_page and amount args in getMakanan

--- src/Resolvers/Makanan/makanan-resolver.ts
+++ src/Resolvers/Makanan/makanan-resolver.ts
@@ -1,7 +1,7 @@
-import { Arg, Field, InputType, Mutation, Query, Resolver } from "type-graphql";
+import { Arg, Field, InputType, Int, Mutation, Query, Resolver } from "type-graphql";
 import { Makanan } from "../../Entity/Makanan/Makanan";
 import { getRepository } from "typeorm";
-import { IsNumber, IsString, MaxLength } from "class-validator";
+import { IsNumber, IsString, Max, MaxLength, Min } from "class-validator";
 import { ApolloError } from "apollo-server-express";
 
 @InputType()
@@ -21,13 +21,32 @@ class MakananInput{
     harga: number
 }
 
+@InputType()
+class getMakananArgs {
+    @Min(1)
+    @Field(type => Int, {nullable: true})
+    skip_page?: number;
+
+    @Min(1)
+    @Max(50)
+    @Field(type => Int, {nullable: true})
+    amount?: number;
+}
+
+
 @Resolver(Makanan)
 export default class MakananResolver{
     constructor(){}
 
     @Query(() => [Makanan])
-    async getAllMakanan(){
-        return await getRepository(Makanan).find();
+    async getAllMakanan(@Arg('args', type => getMakananArgs) makananArgs: getMakananArgs){
+        const amount = makananArgs.amount || 1;
+        const skip_page = (makananArgs.skip_page || 0) * amount;
+
+        return await getRepository(Makanan).find({
+            take: amount,
+            skip: skip_page
+        });
     };
 
     @Query(() => Makanan)
